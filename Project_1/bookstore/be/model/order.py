8e35486cffa5605814f2d3b8119a13eca9e67edf
unknown <3237539022@qq.com>
2024-04-21 12:00:25 +0800

    update cancel_unpaid_orders

--- Project_1/bookstore/be/model/order.py
+++ Project_1/bookstore/be/model/order.py
@@ -1,51 +1,52 @@
 import sqlite3 as sqlite
-import uuid
-import json
-import logging
 from be.model import db_conn
 from be.model import error
+import time
 
-#取消定单功能实现ing
 
 class Order(db_conn.DBConn):
     def __init__(self):
         db_conn.DBConn.__init__(self)
 
-# conn.execute(
-#     "CREATE TABLE IF NOT EXISTS new_order( "
-#     "order_id TEXT PRIMARY KEY, user_id TEXT, store_id TEXT)"
-# )
-
     def cancel_order(
-        self, order_id: str, user_id: str, store_id: str, int
-    ) -> (int, str):
-        order_available="canceled"
-        order_status="canceled"
+            self, order_id: str) -> (int, str):
+        order_available = "canceled"
+        order_status = "canceled"
         try:
-            # if not self.user_id_exist(user_id):
-            #     return error.error_non_exist_user_id(user_id) + (order_id,)
-            if not self.store_id_exist(store_id):
-                return error.error_non_exist_store_id(store_id) + (order_id,)
-
             cursor = self.conn.execute(
-                "SELECT user_id, store_id, order_available from new_order where order_id=?", (order_id,)
+                "SELECT order_available from new_order where order_id = ?", (
+                    order_id,)
             )
             row = cursor.fetchone()
             if row is None:
                 return error.error_non_exist_order_id()
 
-            if row[0] != user_id:
-                return error.error_invalid_user_id()
-
-            if row[1] != store_id:
-                return error.error_invalid_store_id()
-
-            if row[2] != "valid":
+            if row[0] != "valid":
                 return error.error_prossessing_order_id()
 
             cursor = self.conn.execute(
-                "UPDATE new_order SET available = ? and status = ? WHERE order_id = ?",
+                "UPDATE new_order SET available = ?, status = ? WHERE order_id = ?",
                 (order_available, order_status, order_id),
+            )
+
+            self.conn.commit()
+        except sqlite.Error as e:
+            return 528, "{}".format(str(e))
+        except BaseException as e:
+            return 530, "{}".format(str(e))
+
+        return 200, "ok"
+
+    def cancel_unpaid_orders(self) -> (int, str):
+        valid_time = 20
+        current_time = time.time()
+        order_cancel_status = "unpaid"
+        order_available = "canceled"
+        order_status = "canceled"
+        try:
+            cursor = self.conn.execute(
+                "update new_order set available = ?, status = ? and where order_time >= ? and order_time < ? and status = ?", (
+                order_available, order_status, current_time - valid_time*2, current_time - valid_time , order_cancel_status,)
             )
 
             self.conn.commit()
