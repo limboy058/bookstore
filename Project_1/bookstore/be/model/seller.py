d096f88b85c5c2b627f0c63172d88d0209dbec9b
limbo-t <2324899904@qq.com>
2024-04-21 17:51:34 +0800

    后端seller.py与user.py修改为mongodb格式(附带测试)

--- Project_1/bookstore/be/model/seller.py
+++ Project_1/bookstore/be/model/seller.py
@@ -1,7 +1,8 @@
-import sqlite3 as sqlite
-from be.model import error
-from be.model import db_conn
-
+import pymongo
+import error
+import user# 仅用于测试
+import db_conn
+# 未做事务处理
 
 class Seller(db_conn.DBConn):
     def __init__(self):
@@ -23,14 +24,8 @@ class Seller(db_conn.DBConn):
             if self.book_id_exist(store_id, book_id):
                 return error.error_exist_book_id(book_id)
 
-            self.conn.execute(
-                "INSERT into store(store_id, book_id, book_info, stock_level)"
-                "VALUES (?, ?, ?, ?)",
-                (store_id, book_id, book_json_str, stock_level),
-            )
-            self.conn.commit()
-        except sqlite.Error as e:
-            return 528, "{}".format(str(e))
+            ret = self.conn['store'].insert_one({'store_id':store_id,'book_id':book_id,'book_info':book_json_str,'stock_level':stock_level})
+            if not ret.acknowledged:  return 528, "{}".format(str(ret))  
         except BaseException as e:
             return 530, "{}".format(str(e))
         return 200, "ok"
@@ -45,15 +40,9 @@ class Seller(db_conn.DBConn):
                 return error.error_non_exist_store_id(store_id)
             if not self.book_id_exist(store_id, book_id):
                 return error.error_non_exist_book_id(book_id)
-
-            self.conn.execute(
-                "UPDATE store SET stock_level = stock_level + ? "
-                "WHERE store_id = ? AND book_id = ?",
-                (add_stock_level, store_id, book_id),
-            )
-            self.conn.commit()
-        except sqlite.Error as e:
-            return 528, "{}".format(str(e))
+            
+            ret = self.conn['store'].update_one({'store_id':store_id,'book_id':book_id},{'$inc': {'stock_level': add_stock_level}})
+            if not ret.acknowledged:  return 528, "{}".format(str(ret)) 
         except BaseException as e:
             return 530, "{}".format(str(e))
         return 200, "ok"
@@ -64,13 +53,33 @@ class Seller(db_conn.DBConn):
                 return error.error_non_exist_user_id(user_id)
             if self.store_id_exist(store_id):
                 return error.error_exist_store_id(store_id)
-            self.conn.execute(
-                "INSERT into user_store(store_id, user_id)" "VALUES (?, ?)",
-                (store_id, user_id),
-            )
-            self.conn.commit()
-        except sqlite.Error as e:
-            return 528, "{}".format(str(e))
+            
+            ret = self.conn['user_store'].insert_one({'store_id':store_id,'user_id':user_id})
+            if not ret.acknowledged:  return 528, "{}".format(str(ret))
         except BaseException as e:
             return 530, "{}".format(str(e))
         return 200, "ok"
+
+if __name__ == "__main__":
+    tmp=Seller()
+    us=user.User()
+    print(us.register('uid1','333'))
+    for item in tmp.conn['user'].find():
+        print(item)
+    
+    print(tmp.create_store('uid2','sid1'))
+    print(tmp.create_store('uid1','sid1'))
+    for item in tmp.conn['user_store'].find():
+        print(item)
+
+    print(tmp.add_book('uid1','sid2','bid1','hellow orld',10))
+    print(tmp.add_book('uid2','sid1','bid3','hellow orld',10))
+
+    print(tmp.add_book('uid1','sid1','bid1','hellow orld',10))
+    for item in tmp.conn['store'].find():
+        print(item)
+
+    print(tmp.add_stock_level('uid1','sid1','bid1',10))
+    for item in tmp.conn['store'].find():
+        print(item)
+    
