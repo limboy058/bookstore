4bdc08b53d158ded3cbfeba69696c6b9083afa3c
unknown <3237539022@qq.com>
2024-04-24 18:29:36 +0800

    more test forsearch order

--- Project_1/bookstore/fe/test/test_search_order.py
+++ Project_1/bookstore/fe/test/test_search_order.py
@@ -3,6 +3,7 @@ import pytest
 from fe.test.gen_book_data import GenBook
 from fe.access.new_buyer import register_new_buyer
 import uuid
+import random
 
 
 class TestCancelOrder:
@@ -25,3 +26,28 @@ class TestCancelOrder:
         assert code == 200
         code, order_list= self.buyer.search_order()
         assert code == 200
+
+    def test_one_order_ok(self):
+        ok, buy_book_id_list = self.gen_book.gen(
+            non_exist_book_id=False, low_stock_level=False
+        )
+        assert ok
+        code, order_id = self.buyer.new_order(self.store_id, buy_book_id_list)
+        assert code == 200
+        order_list=list()
+        order_list.append(order_id)
+        code, received_list= self.buyer.search_order()
+        assert order_list == received_list
+
+    def test_many_orders_ok(self):
+        ok, buy_book_id_list = self.gen_book.gen(
+            non_exist_book_id=False, low_stock_level=False
+        )
+        assert ok
+        order_list=list()
+        for i in range(0,random.randint(1, 10)):
+            code, order_id = self.buyer.new_order(self.store_id, buy_book_id_list)
+            assert code == 200
+            order_list.append(order_id)
+        code, received_list= self.buyer.search_order()
+        assert order_list == received_list
