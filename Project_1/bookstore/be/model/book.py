35841713f7249507d9430e67b735b5b2a0649aa8
小人鱼 <1823748191@qq.com>
2024-05-26 23:07:49 +0800

    update add fund function and search book function

--- Project_1/bookstore/be/model/book.py
+++ Project_1/bookstore/be/model/book.py
@@ -1,4 +1,5 @@
 import pymongo
+import psycopg2
 import json
 import logging
 import pymongo.errors
@@ -34,72 +35,100 @@ class searchBook(db_conn.DBConn):
         order_by_method=None,  # [stock_level, sales, pub_time, price] + [1,-1]  1 means increasingly and -1 means decreasingly
         having_stock=None,
     ):
-        books = []
-        conditions = {}
+        books = list()
+        query="select * from book_info "
+        conditions = " where "
+        fill=list()
         try:
             if (foozytitle != None):
-                conditions['$text'] = {'$search': foozytitle}
+                conditions+=" title @>%s and"
+                fill.append(foozytitle)
             if (reqtags != None):
-                conditions['book_info.tags'] = {'$all': list(reqtags)}
+                conditions+= " tags@> %s and"
+                fill.append(reqtags)
             if (id != None):
-                conditions['book_info.id'] = str(id)
+                conditions+=" id=%s and"
+                fill.append(id)
             if (isbn != None):
-                conditions['book_info.isbn'] = str(isbn)
+                conditions+=" isbn=%s and"
+                fill.append(isbn)
             if (author != None):
-                conditions['book_info.author'] = str(author)
+                conditions+=" author=%s and"
+                fill.append(author)
             if (lowest_price != None):
-                conditions['book_info.price'] = {'$gte': lowest_price}
+                conditions+=" price>=%s and"
+                fill.append(lowest_price)
             if (highest_price != None):
-                if (lowest_price != None):
-                    conditions['book_info.price']['$lte'] = highest_price
-                else:
-                    conditions['book_info.price'] = {'$lte': highest_price}
+                conditions+=" price<=%s and"
+                fill.append(highest_price)
             if (store_id != None):
-                conditions['store_id'] = str(store_id)
+                conditions+=" store_id=%s and"
+                fill.append(store_id)
             if (lowest_pub_year != None):
-                conditions['book_info.pub_year'] = {'$gte': lowest_pub_year}
+                conditions+=" pub_year>=%s and"
+                fill.append(lowest_pub_year)
             if (highest_pub_year != None):
-                highest_pub_year = str(int(highest_pub_year) + 1)
-                if (lowest_pub_year != None):
-                    conditions['book_info.pub_year']['$lt'] = highest_pub_year
-                else:
-                    conditions['book_info.pub_year'] = {
-                        '$lt': highest_pub_year
-                    }
+                conditions+=" pub_year<=%s and"
+                fill.append(highest_pub_year)
             if (publisher != None):
-                conditions['book_info.publisher'] = str(publisher)
+                conditions+=" publisher=%s and"
+                fill.append(publisher)
             if (translator != None):
-                conditions['book_info.translator'] = str(translator)
+                conditions+=" translator=%s and"
+                fill.append(translator)
             if (binding != None):
-                conditions['book_info.binding'] = str(binding)
+                conditions+=" binding=%s and"
+                fill.append(binding)
             if (having_stock != None and having_stock == True):
-                conditions['stock_level'] = {'$gt': 0}
-            sort = []
+                condition+="stock_level>=0 and"
+
+            if(len(conditions)>7):
+                conditions=conditions[:-3]
+            else:
+                conditions=" "
             if (order_by_method != None):
                 if (order_by_method[0] not in self.order_by_conditions or
                     (order_by_method[1] != 1 and order_by_method[1] != -1)):
                     return 522, error.error_illegal_order_condition(
                         str(order_by_method[0] + ' ' + order_by_method[1])), ""
-                if (order_by_method[0] == 'price'
-                        or order_by_method[0] == 'pub_year'):
-                    order_by_method[0] = 'book_info.' + order_by_method[0]
-                sort.append((order_by_method[0], order_by_method[1]))
-            cursor = self.conn['store'].find(conditions,
-                                             limit=page_size,
-                                             skip=page_size * page_no,
-                                             sort=sort)
-            for row in cursor:
-                row['book_info']['store_id'] = row['store_id']
-                row['book_info']['stock_level'] = row['stock_level']
-                row['book_info']['sales'] = row['sales']
-                books.append(row)
-        except pymongo.errors.PyMongoError as e:
+                conditions+=" order by %s %s"
+                fill.append(order_by_method[0])
+                if(order_by_method[1]==1):
+                    fill.append("asc")
+                else:
+                    fill.append("desc")
+            
+            conn=self.get_conn()
+            cur=conn.cursor()
+            cur.execute(query+conditions+" limit "+str(page_size)+" skip "+str(page_no*page_size),fill)
+            res=cur.fetchall()
+            for row in res:
+                book=dict()
+                book['book_id']=row[0]
+                book['store_id']=row[1]
+                book['price']=row[2]
+                book['stock_level']=row[3]
+                book['sales']=row[4]
+                book['title']=row[5]
+                book['author']=row[6]
+                book['publisher']=row[7]
+                book['original_title']=row[8]
+                book['translator']=row[9]
+                book['pub_year']=row[10]
+                book['pages']=row[11]
+                book['currency_unit']=row[12]
+                book['binding']=row[13]
+                book['isbn']=row[14]
+                book['author_intro']=row[15]
+                book['book_intro']=row[16]
+                book['content']=row[17]
+                book['picture']=row[18]
+                books.append(json.dumps(book['book_info']))
+        except psycopg2.Error as e:
             logging.info("528, {}".format(str(e)))
             return 528, "{}".format(str(e)), ""
         except BaseException as e:
             logging.info("530, {}".format(str(e)))
             return 530, "{}".format(str(e)), ""
-        lst = list()
-        for i in books:
-            lst.append(json.dumps(i['book_info']))
-        return 200, "ok", lst
+        
+        return 200, "ok", books
