175ff57d4c88cfa929a1a9cebfa6c5996dad184b
limbo-t <2324899904@qq.com>
2024-05-28 00:36:30 +0800

    scanner

--- Project_1/bookstore/be/model/scanner.py
+++ Project_1/bookstore/be/model/scanner.py
@@ -1,7 +1,10 @@
 #live_time订单有效时间默认1200,scan_interval为扫描间隔默认10  单位均为秒
 
 import time, datetime
-import pymongo.errors
+import psycopg2
+
+import sys
+sys.path.append(r'D:\DS_bookstore\Project_1\bookstore')
 from be.model import db_conn
 
 
@@ -14,65 +17,52 @@ class Scanner(db_conn.DBConn):
 
     def keep_running(self, keep=False):
         t = 0
-        while t < 10:
-            session = self.client.start_session()
-            session.start_transaction()
-            try:
-                # ret=self.conn['new_order'].find()
-                # for item in ret:
-                #     print('此订单时间为',datetime.datetime.fromtimestamp(item['order_time']),'状态为',item['status'])
-                cur_time = int(time.time())
-                cursor = self.conn['new_order'].find(
-                    {
-                        'order_time': {
-                            '$gte':
-                            cur_time - self.live_time - self.scan_interval,
-                            '$lt':
-                            cur_time - self.live_time + self.scan_interval
-                        },
-                        'status': 'unpaid'
-                    }, {
-                        'order_id': 1,
-                        'store_id': 1,
-                        'detail': 1
-                    },
-                    session=session)
-                for i in cursor:
-                    detail = list(i['detail'])
-                    store_id = i['store_id']
-                    for j in detail:
-                        self.conn['store'].update_many(
-                            {
-                                'store_id': store_id,
-                                'book_id': j[0]
-                            }, {'$inc': {
-                                'stock_level': j[1],
-                                'sales': -j[1]
-                            }},
-                            session=session)
-                ret = self.conn['new_order'].update_many(
-                    {
-                        'order_time': {
-                            '$gte':
-                            cur_time - self.live_time - self.scan_interval,
-                            '$lt':
-                            cur_time - self.live_time + self.scan_interval
-                        },
-                        'status': 'unpaid'
-                    }, {'$set': {
-                        'status': 'canceled'
-                    }},
-                    session=session)
-                yield 200, ret.modified_count
-                #return
-            except pymongo.errors.PyMongoError as e:
-                yield 528, "{}".format(str(e))
-                return
-            except Exception as e:
-                yield 530, "{}".format(str(e))
-                return
-            session.commit_transaction()
-            session.end_session()
-            time.sleep(self.scan_interval)
-            if not keep:
-                t += 1
+        
+        try:
+            with self.get_conn() as conn:
+                while t < 10:
+                    with conn.cursor() as cur:
+                        conn.autocommit = False
+
+                        cur_time = datetime.datetime.now()
+                        cur.execute('''update new_order 
+                                    set status=%s 
+                                    where status=%s and time>=%s and time <%s 
+                                    returning order_id,store_id
+                                    ''',
+                                    ('canceled','unpaid',
+                                     cur_time-datetime.timedelta(seconds=self.live_time+self.scan_interval),
+                                     cur_time+datetime.timedelta(seconds=self.live_time+self.scan_interval),))
+                        ret=cur.fetchall()
+                        cnt=cur.rowcount
+                        for item in ret:
+                            cur.execute('''update book_info 
+                                        set stock_level=stock_level+count,sales=sales-count 
+                                        from order_detail
+                                        where store_id=%s and book_info.book_id=order_detail.book_id and order_id=%s
+                                        '''
+                                        ,(item[1],item[0]))
+
+                        conn.commit()
+                        yield 200, cnt
+                time.sleep(self.scan_interval)
+                if not keep:
+                    t += 1
+        except psycopg2.Error as e:
+            yield 528, "{}".format(str(e))
+            return
+        except BaseException as e:
+            yield 530, "{}".format(str(e))
+            return
+        
+if __name__=='__main__':
+    s=Scanner(live_time=10,scan_interval=2)
+    g = s.keep_running()
+
+    while(1):
+        time.sleep(1)
+        try:
+            s = next(g)
+            print(s)
+        except:
+            assert 0
